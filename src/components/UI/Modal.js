ebd08cf3a6c9d1595841d08d26570beb36287062
Kevser Ipek <kevseripek@Kevser-MacBook-Air.local>
2022-07-20 15:50:40 +0300

    Add cart and modal component

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,40 @@
+import { Fragment } from "react";
+import ReactDOM from "react-dom";
+
+import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClose}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlays");
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElement
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+};
+
+export default Modal;
+
+// Portallar ne ise yariyor neden burda portal kullandik?
+// Portalsiz yapamaz miydik
+// Sadece o portal icinde render ediliyormus sanirim
+// KAFAMDA DELI SORULARRRR DID DI DIRIRIRIMMM
